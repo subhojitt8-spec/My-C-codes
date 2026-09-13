2a7d29707f3b043d895b1070d91e78b4fa5efb65
Suprio Adhikary <suprioadhikary802@gmail.com>
2026-09-13 23:19:56 +0530

    optimized code of program81

--- 81akash.c
+++ 81akash.c
@@ -1,21 +1,31 @@
-#include<stdio.h>
-int main(){
-    int  N,i=1, count=0,p=0, m;
-    printf("Enter nth number of prime number which you want:");
-    scanf("%d", &m);
-    for(i=2;i<= 9999999999;i++){ 
-        count=0;
-        for(int j=1;j<=i;j++){
-            if(i%j==0)
-                count++;
-        }
-            if(count==2)
-            p+=1;
-            if(p==m){
-            printf("%d", i);
-            break;
-            }
-    }
-         return 0;
-       }
-    
+#include <stdio.h>
+int main()
+{
+int c=0,prime_position=0,num;
+printf("Enter a number : ");
+scanf("%d",&num);
+int i=1;
+while(1)
+{
+for(int j=1;j<=i;j++)
+{
+if(i%j==0)
+c++;
+
+}
+if(c==2)
+{
+prime_position++;
+if(prime_position==num){
+printf("The prime number at the %dth position is : %d\n",prime_position,i);
+break;
+}
+
+}
+c=0;
+i++;
+}
+
+return 0;
+
+}

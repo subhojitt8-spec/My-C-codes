a0d245f317cab3bb26a41e5161af47a2540cd0c4
subhojitt8-spec <subhojitt8@gmail.com>
2026-09-14 09:06:22 +0530

    Finding HCF of two number

---
+++ HCF OF 2 NUMBERS.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int m,n, hcf=0;
+    printf("Enter two numbes to find their HCF:");
+    scanf("%d", &m);
+     scanf("%d", &n);
+    for(int i=1;i<=((m<n)?m:n);i++){
+        if(m%i==0 && n%i==0)
+        hcf=i;
+    }
+    printf("The HCF of %d and %d is %d:", m,n, hcf);
+    return 0;
+}
+            

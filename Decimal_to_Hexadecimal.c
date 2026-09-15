659b7e0928ea6b2f700a806c5439d10956090753
subhojitt8-spec <subhojitt8@gmail.com>
2026-09-15 19:13:23 +0530

    Decimal to Hexadecimal code pushed.

---
+++ Decimal_to_Hexadecimal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int num;
+    printf("Entr a number:");
+    scanf("%d", &num);
+    printf("The decimal to hexadecimal conversion of %d is %x", num,num);
+    
+}

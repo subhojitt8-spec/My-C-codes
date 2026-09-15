e437c1d70a659005430dc7d3b05892fca45bd693
subhojitt8-spec <subhojitt8@gmail.com>
2026-09-15 19:11:50 +0530

    Decimal to Octal code pushed

---
+++ Deciaml_to_Octal.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int  main(){
+    int num;
+    printf("Entr a number:");
+    scanf("%d", &num);
+    printf("The decimal to octal conversion of %d is %o", num,num);
+     return 0;
+}
